4a4e917fd5a76cd23e8592e364987c26769f11c5
mcarpent87 <mclean.carpenter@lendio.com>
2023-04-12 16:07:03 -0600

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+    <p>Bacon ipsum dolor amet ham spare ribs burgdoggen fatback jerky rump turkey beef ribs chislic buffalo ball tip beef. Turkey bresaola boudin filet mignon beef ribs andouille spare ribs fatback sausage shankle jowl. Shank venison kielbasa ham, corned beef short ribs andouille hamburger. Fatback sausage alcatra chuck.
+
+        Frankfurter tongue corned beef short loin filet mignon kevin pastrami, turkey sirloin sausage jowl. Venison pig beef jerky meatloaf ham. Pork loin tongue alcatra drumstick ball tip rump shankle sausage bresaola kevin. Filet mignon bresaola corned beef tenderloin, salami chislic flank. Porchetta filet mignon buffalo, turkey flank pancetta jowl prosciutto. Pancetta ham turkey pork loin rump meatball filet mignon pork belly drumstick swine.
+        
+        Meatball doner bresaola, swine drumstick biltong ground round filet mignon pastrami meatloaf spare ribs. Tail beef ribs shank filet mignon, jerky ham short loin spare ribs tri-tip hamburger. Spare ribs bacon ground round, venison tail chicken meatloaf pig. Alcatra short ribs landjaeger chuck, burgdoggen sirloin jowl boudin venison bacon corned beef turducken tongue. Tail turkey strip steak ham picanha tenderloin pork flank pork loin leberkas ball tip jowl ham hock prosciutto.</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+    <p>Bacon ipsum dolor amet ham spare ribs burgdoggen fatback jerky rump turkey beef ribs chislic buffalo ball tip beef. Turkey bresaola boudin filet mignon beef ribs andouille spare ribs fatback sausage shankle jowl. Shank venison kielbasa ham, corned beef short ribs andouille hamburger. Fatback sausage alcatra chuck.
+
+        Frankfurter tongue corned beef short loin filet mignon kevin pastrami, turkey sirloin sausage jowl. Venison pig beef jerky meatloaf ham. Pork loin tongue alcatra drumstick ball tip rump shankle sausage bresaola kevin. Filet mignon bresaola corned beef tenderloin, salami chislic flank. Porchetta filet mignon buffalo, turkey flank pancetta jowl prosciutto. Pancetta ham turkey pork loin rump meatball filet mignon pork belly drumstick swine.
+        
+        Meatball doner bresaola, swine drumstick biltong ground round filet mignon pastrami meatloaf spare ribs. Tail beef ribs shank filet mignon, jerky ham short loin spare ribs tri-tip hamburger. Spare ribs bacon ground round, venison tail chicken meatloaf pig. Alcatra short ribs landjaeger chuck, burgdoggen sirloin jowl boudin venison bacon corned beef turducken tongue. Tail turkey strip steak ham picanha tenderloin pork flank pork loin leberkas ball tip jowl ham hock prosciutto.</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+    <p>Bacon ipsum dolor amet ham spare ribs burgdoggen fatback jerky rump turkey beef ribs chislic buffalo ball tip beef. Turkey bresaola boudin filet mignon beef ribs andouille spare ribs fatback sausage shankle jowl. Shank venison kielbasa ham, corned beef short ribs andouille hamburger. Fatback sausage alcatra chuck.
+
+        Frankfurter tongue corned beef short loin filet mignon kevin pastrami, turkey sirloin sausage jowl. Venison pig beef jerky meatloaf ham. Pork loin tongue alcatra drumstick ball tip rump shankle sausage bresaola kevin. Filet mignon bresaola corned beef tenderloin, salami chislic flank. Porchetta filet mignon buffalo, turkey flank pancetta jowl prosciutto. Pancetta ham turkey pork loin rump meatball filet mignon pork belly drumstick swine.
+        
+        Meatball doner bresaola, swine drumstick biltong ground round filet mignon pastrami meatloaf spare ribs. Tail beef ribs shank filet mignon, jerky ham short loin spare ribs tri-tip hamburger. Spare ribs bacon ground round, venison tail chicken meatloaf pig. Alcatra short ribs landjaeger chuck, burgdoggen sirloin jowl boudin venison bacon corned beef turducken tongue. Tail turkey strip steak ham picanha tenderloin pork flank pork loin leberkas ball tip jowl ham hock prosciutto.</p>
+    </article>
+</body>
